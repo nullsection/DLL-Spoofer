5b927775dc679739db445e74e94cbe3d65eb81e0
nullsection <46195001+nullsection@users.noreply.github.com>
2025-03-05 01:03:44 +1100

    added DLL Proxy support via pragma

--- spoof.py
+++ spoof.py
@@ -1,4 +1,6 @@
 import pefile
+import os 
+import argparse
 
 def get_exported_functions(dll_path):
     pe = pefile.PE(dll_path)
@@ -10,27 +12,34 @@ def get_exported_functions(dll_path):
 
     return exported_functions
 
-def generate_cpp_file(dll_path, output_cpp_file, exported_functions):
+def generate_cpp_file(dll_path, output_cpp_file, exported_functions, messagebox):
     with open(output_cpp_file, "w") as cpp_file:
+        dll_no_ext = os.path.splitext(os.path.basename(dll_path))[0]
         cpp_file.write("// dllmain.cpp : Defines the entry point for the DLL application.\n")
         cpp_file.write("#include \"pch.h\"\n")
         cpp_file.write("#include <iostream>\n")
         cpp_file.write("#include <Windows.h>\n")
         cpp_file.write("#include <windows.h>\n\n")
-        cpp_file.write("extern \"C\" {\n\n")
-
-        for func_name in exported_functions:
-            cpp_file.write(f"__declspec(dllexport) void {func_name}() {{\n")
-            cpp_file.write(f'    MessageBox(NULL, L"{func_name} called", L"Function Call", MB_OK);\n')
+        
+        if not messagebox:
+            for func_name in exported_functions:
+                cpp_file.write(f'#pragma comment(linker, "/export{func_name}={dll_no_ext}.{func_name}")\n')
+                cpp_file.write("\n")
+        else:
+            cpp_file.write("extern \"C\" {\n\n")
+            for func_name in exported_functions:
+                
+                cpp_file.write(f"__declspec(dllexport) void {func_name}() {{\n")
+                cpp_file.write(f'    MessageBox(NULL, L"{func_name} called", L"Function Call", MB_OK);\n')
+                
+                cpp_file.write("}\n\n")
             cpp_file.write("}\n\n")
-
-        cpp_file.write("}\n\n")
 
         # DllMain function
         cpp_file.write("BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {\n")
         cpp_file.write("    switch (ul_reason_for_call) {\n")
         cpp_file.write("    case DLL_PROCESS_ATTACH:\n")
-        cpp_file.write(f"\t\t{exported_functions[0]}();\n")
+        #cpp_file.write(f"\t\t{exported_functions[0]}();\n")
         cpp_file.write("    case DLL_THREAD_ATTACH:\n")
         cpp_file.write("    case DLL_THREAD_DETACH:\n")
         cpp_file.write("    case DLL_PROCESS_DETACH:\n")
@@ -42,16 +51,17 @@ def generate_cpp_file(dll_path, output_cpp_file, exported_functions):
         print("Saved Spoofed Template.")
 
 def main():
-    import argparse
+    
 
     parser = argparse.ArgumentParser(description="Generate a C++ source file for a DLL that displays MessageBox for exported functions.")
     parser.add_argument("dll_path", help="Path to the DLL to inspect and create the C++ source for.")
+    parser.add_argument("--messagebox",action="store_true",default=False, help="If set, MessageBoxA all exports instead of proxy")
     args = parser.parse_args()
 
     output_cpp_file = "ExportedFunctions.cpp"  # Output C++ source file
 
     exported_functions = get_exported_functions(args.dll_path)
-    generate_cpp_file(args.dll_path, output_cpp_file, exported_functions)
+    generate_cpp_file(args.dll_path, output_cpp_file, exported_functions, args.messagebox)
 
 if __name__ == "__main__":
     main()
